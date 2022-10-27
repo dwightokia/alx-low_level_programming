583585c7da50bef314c56104b02258d07193fa7d
Okia Dwight <gabrielokia5@gmail.com>
2022-10-27 13:37:21 -0700

    concentrate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,8 +5,10 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src)
+int main()
 {
+	char *_strcat(char *dest, char *src)
+
 	int count = 0, count2 = 0;
 
 	while (*(dest + count) != '\0')
